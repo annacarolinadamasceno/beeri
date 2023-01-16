671f4d6577ca9e6bfd9221ff4c56a2df59271583
Sebastião Moura <sebastiaoarmdev@gmail.com>
2023-01-16 17:40:50 -0300

    :construction: adicionando arquivos

---
+++ back/index.php
@@ -0,0 +1,24 @@
+<?php 
+	$beerCount = 0;
+?>
+<html>
+	<head>
+		<style>
+			* {
+				font-family: monospace;
+			}
+		</style>
+		<title>Beeri</title>
+	</head>
+	<body>
+		<header>
+			
+		</header>
+		<main>
+			<p>Cervejas: <?=($beerCount)?></p>
+		</main>
+		<footer>
+
+		</footer>
+	</body>
+</html>
