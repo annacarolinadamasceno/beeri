e13750a7101b936fe3ebc9826300e413ecaf962c
Sebastião Moura <97861831+sebastiaoarmdev@users.noreply.github.com>
2023-01-25 19:45:39 -0300

    :sparkles: acrescenta opção de dividir a conta

--- js/script.js
+++ js/script.js
@@ -1,23 +1,94 @@
-var unitValue = 0;
 var beerCount = 0;
-var beerTotalValue = 0;
+var beerValue = 0;
+var beerTotalValue = beerValue * beerCount;
+var formattedBeerTotalValue = beerTotalValue.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+var beerTypes = [
+	'images/bottle.png',
+	'images/can.png', 
+	'images/drink.png', 
+	'images/glass.png', 
+	'images/mug.png'];
+var beerType = beerTypes[0];
+var payerCount = 1;
+var beerValuePerPayer = beerTotalValue/payerCount;
+var formattedBeerValuePerPayer = beerValuePerPayer.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
 
-const printValues = () => {
-    document.getElementById("beerCount").innerHTML = "<p> Total de biritas: " + beerCount + "</p>";
-    unitValue = document.getElementById("beerUnitValue").value;
-    beerTotalValue = beerCount * unitValue;
-    document.getElementById("totalValue").innerHTML = "<p> Total a pagar: R$ " + beerTotalValue.toFixed(2) + "</p>";
-    document.getElementById("graphicBeerCount").innerHTML = "";
-    for (let i = 1; i <= beerCount; i++) {
-        document.getElementById("graphicBeerCount").innerHTML += '<img src="./assets/images/beer.png" style="width: 254px;">';
-    }
+function updateGraphicBeerTypes() {
+	let divGraphicBeerTypesInnerHTML = '';
+	for (let i = 0; i < beerTypes.length; i++) {
+		let status = beerType == beerTypes[i] ? 'on' : 'off';
+		divGraphicBeerTypesInnerHTML = divGraphicBeerTypesInnerHTML + `<img src="${beerTypes[i]}" class="button ${status}" onclick="updateBeerType(this)">`;
+	}
+	document.getElementById('graphicBeerTypes').innerHTML = divGraphicBeerTypesInnerHTML;
 }
 
-const beerCounterPlus = () => {
-    beerCount = beerCount + 1;
-    printValues();
+function updateBeerType(element) {
+	beerType = element.src;
+	let types = document.getElementsByClassName('button');
+	for (let i = 0; i < types.length; i++) {
+		types[i].classList.replace('on', 'off');
+	}
+	element.classList.replace('off', 'on');
+	let beers = document.getElementsByClassName('graphicBeer');
+	for (let i = 0; i < beers.length; i++) {
+		beers[i].src = beerType;
+	}
 }
-const beerCounterMinus = () => {
-    beerCount = (beerCount > 0) ? beerCount - 1 : beerCount;
-    printValues();
-}	
+
+function updateBeerValuePerPayer() {
+	let divBeerValuePerPayerInnerHTML = '';
+	beerValuePerPayer = beerTotalValue/payerCount;
+	formattedBeerValuePerPayer = beerValuePerPayer.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+	divBeerValuePerPayerInnerHTML = divBeerValuePerPayerInnerHTML + `${formattedBeerValuePerPayer}`;
+	document.getElementById('beerValuePerPayer').innerHTML = divBeerValuePerPayerInnerHTML;
+}
+
+function updateBeerTotalValue() {
+	let divBeerTotalInnerHTML = '';
+	beerTotalValue = beerValue * beerCount;
+	formattedBeerTotalValue = beerTotalValue.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+	divBeerTotalInnerHTML = formattedBeerTotalValue;
+	document.getElementById('beerTotal').innerHTML = divBeerTotalInnerHTML;
+	updateBeerValuePerPayer();
+}
+
+function updateGraphicBeerCount() {
+	let divGraphicCountInnerHTML = '';
+	for (let i = 0; i < beerCount; i++) {
+		divGraphicCountInnerHTML = divGraphicCountInnerHTML + `<img class="graphicBeer" src="${beerType}">`;
+	}
+	updateBeerTotalValue();
+	document.getElementById('graphicBeerCount').innerHTML = divGraphicCountInnerHTML;
+	document.getElementById('beerCount').innerHTML = beerCount;
+}
+
+function updateBeerCount(increment) {
+	beerCount = beerCount + increment;
+	updateGraphicBeerCount();
+}
+
+function addABeer() {
+	updateBeerCount(+1);
+}
+
+function removeABeer() {
+	if (beerCount > 0) {
+		updateBeerCount(-1);
+	}
+}
+
+function updateBeerValue(newValue) {
+	beerValue = newValue;
+	updateBeerTotalValue();
+}
+function updatePayerCount(newValue) {
+	payerCount = newValue;
+	updateBeerValuePerPayer();
+}
+
+function start() {
+	console.log('Started');
+	updateGraphicBeerTypes();
+}
+
+document.onload = start();
